dbdbb8b2ce82eae580ce3b24b3bf8edc60e4b95d
alvropena <me@alvropena.com>
2024-09-07 21:32:49 -0700

    Update getFormattedDate function to include notification timestamp

--- components/notifications/variants/notification-achievement.tsx
+++ components/notifications/variants/notification-achievement.tsx
@@ -1,23 +1,15 @@
-import { format, isToday, isYesterday } from "date-fns";
-import { NotificationAchievementType } from "../types/notification-achievement-type";
 import { getFormattedDate } from "../../../lib/format-date";
+import { NotificationAchievementType } from "../types/notification-achievement-type";
 
 export function NotificationAchievement({ notification, onMarkAsRead }: NotificationAchievementType) {
     if (notification.type !== 'achievement') {
         return null;
     }
 
-    const getFormattedDate = () => {
-        const notificationDate = new Date(notification.timestamp);
-        if (isToday(notificationDate)) return format(notificationDate, 'p');
-        if (isYesterday(notificationDate)) return format(notificationDate, 'p');
-        return format(notificationDate, 'MM/dd hh:mm a');
-    };
-
     return (
         <div className="relative flex flex-col p-2 border-b" onClick={() => onMarkAsRead(notification.id)}>
             <div className="flex justify-between items-center">
-                <small className="text-gray-500">{getFormattedDate()}</small>
+                <small className="text-gray-500">{getFormattedDate(notification.timestamp)}</small>
             </div>
             <div className="mt-1">
                 <p>{notification.message}</p>
